5ff45257d5258a54711107c7a472424aaff24c9d
buksnamasna <slavko.inc@gmail.com>
2017-08-11 12:08:33 +0200

    Revert "nova izmjena, najnovija od najnovijih"
    This reverts commit 9f2fc2eb1f75feee74d298ef08c083944108f7aa.

--- main.c
+++ main.c
@@ -5,6 +5,5 @@ int main()
 	printf("Hello world!\n");
 	printf("uradi nesto------------------\n");
 	printf("prikazuje nesto drugo");
-	printf("promjenjena linija");
-	printf("jos jednda izmjena");
+	printf("promjenjena linija")
 }
