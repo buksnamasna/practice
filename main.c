9f2fc2eb1f75feee74d298ef08c083944108f7aa
buksnamasna <slavko.inc@gmail.com>
2017-08-11 12:05:49 +0200

    nova izmjena, najnovija od najnovijih

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main()
 	printf("Hello world!\n");
 	printf("Neki tamo novi printf\n");
 	printf("prikazuje nesto drugo");
-	printf("promjenjena linija")
+	printf("promjenjena linija");
+	printf("jos jednda izmjena");
 }
